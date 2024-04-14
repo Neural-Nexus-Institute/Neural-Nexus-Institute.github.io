dd15132c59022f573e907088f8527ebfb7239f8c
Taha <TahaMunawar43@outlook.com>
2024-04-14 20:31:31 +0500

    updated website

---
+++ _app/immutable/chunks/loader.5VIA49nm.js
@@ -0,0 +1,1 @@
+import{c as r}from"./styles.Yu2VhoOj.js";import{g as t}from"./entry.xGd0MZdO.js";function a(o){r.update(e=>({...e,cover:"covered",loader:"running"})),setTimeout(()=>{t(o)},400)}export{a as h};
---
+++ _app/immutable/entry/start.D6x8L5A1.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.xGd0MZdO.js";export{t as start};
