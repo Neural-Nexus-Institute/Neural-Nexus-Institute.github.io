8bddd7bdd852f8aebd8d22e8b92b4fb2f4206f1b
Taha ibn Munawar <taham@alpine.my.domain>
2024-09-13 19:18:19 +0500

    Fixed Seo of Hero page, and added scrolling heading

---
+++ _app/immutable/chunks/loader.hgfKuhHx.js
@@ -0,0 +1,1 @@
+import{c as r}from"./styles.DlI97WV4.js";import{g as t}from"./entry.DDoPU7Ue.js";function a(o){r.update(e=>({...e,cover:"covered",loader:"running"})),setTimeout(()=>{t(o)},400)}export{a as h};
---
+++ _app/immutable/entry/start.CRjpE73o.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DDoPU7Ue.js";export{t as start};
